5ea57d2e2a19c44cfdd5eef8f3ddfc171d2caabe
LasithEshan <lasitheshan1994@gmail.com>
2018-05-17 07:38:41 +0530

    added libraries

--- app.js
+++ app.js
@@ -5,7 +5,11 @@ request({ //request(object, callback)
     json: true
 }, (error, response, body)=>{
 
-    console.log(JSON.stringify(body, undefined, 2));
+    // console.log(JSON.stringify(response, undefined, 2));
+    console.log(`Address   : ${body.results[0].formatted_address}`);
+    console.log(`Latitude  : ${body.results[0].geometry.location.lat}`);
+    console.log(`Longitude : ${body.results[0].geometry.location.lng}`);
+    
 
 });
 
