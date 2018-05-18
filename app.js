25285ec7d46422dc67f3308c250a47f92a5bcf56
LasithEshan <lasitheshan1994@gmail.com>
2018-05-18 09:54:09 +0530

    error handling

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var argv = yargs
     .alias('help','h')
     .argv;
 debugger;
-    var encodedAddress = encodeURIComponent(argv.a);
+    var encodedAddress = encodeURIComponent(argv.address);
     console.log(encodedAddress);
 
 request({ //request(object, callback)
@@ -23,10 +23,19 @@ request({ //request(object, callback)
     json: true
 }, (error, response, body)=>{
 
+    if(error){
+        console.log('Unable to connect to Server');
+    }else if(body.status === 'ZERO_RESULTS'){
+        console.log('Address not found');
+    }else if(body.status === 'OK'){
+
+        console.log(`Address   : ${body.results[0].formatted_address}`);
+        console.log(`Latitude  : ${body.results[0].geometry.location.lat}`);
+        console.log(`Longitude : ${body.results[0].geometry.location.lng}`);
+       
+    }
+
     // console.log(JSON.stringify(response, undefined, 2));
-    console.log(`Address   : ${body.results[0].formatted_address}`);
-    console.log(`Latitude  : ${body.results[0].geometry.location.lat}`);
-    console.log(`Longitude : ${body.results[0].geometry.location.lng}`);
     
 
 });
